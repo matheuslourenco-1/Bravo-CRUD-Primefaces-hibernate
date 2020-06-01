4c2dbe9669ae57c72307b582c47a1717f3caa5c6
matheuslourenco-1 <57508423+matheuslourenco-1@users.noreply.github.com>
2020-06-01 19:44:48 -0300

    Criado Classes faltantes Gerenciador

---
+++ src/java/br/com/senacrs/DAO/GerenciadorDAO.java
@@ -0,0 +1,35 @@
+package br.com.senacrs.DAO;
+
+import br.com.senacrs.TO.Gerenciador;
+import br.com.senacrs.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+
+
+public class GerenciadorDAO {
+    Session session;
+
+    public GerenciadorDAO() {
+        session = HibernateUtil.getSessionFactory().openSession();
+    }
+    
+    public List<Gerenciador> listar(){
+        List<Gerenciador> gerenciador = session.createCriteria(Gerenciador.class).list();
+        session.close();
+        return gerenciador;
+    }
+    public void remover(Object gerenciador){
+        session.beginTransaction();
+        session.delete(gerenciador);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public void salvar(Gerenciador gerenciador) {
+        session.beginTransaction();
+        session.saveOrUpdate(gerenciador);
+        session.getTransaction().commit();
+        session.close();
+    }
+    
+}
---
+++ src/java/br/com/senacrs/controller/GerenciadorController.java
@@ -0,0 +1,43 @@
+package br.com.senacrs.controller;
+
+import br.com.senacrs.DAO.GerenciadorDAO;
+import br.com.senacrs.TO.Gerenciador;
+import java.io.Serializable;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean
+@SessionScoped
+public class GerenciadorController implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private Gerenciador cliente = new Gerenciador();
+    
+    
+    
+    public Gerenciador getGerenciador() {
+        return cliente;
+    }
+
+    public void setGerenciador(Gerenciador cliente) {
+        this.cliente = cliente;
+    }
+    //Metodo Salvar
+    public void salvar(){
+        GerenciadorDAO client = new GerenciadorDAO();
+        client.salvar(cliente);
+        cliente = new Gerenciador();
+    }
+    //metodo listar
+    public List<Gerenciador> listar(){
+        GerenciadorDAO client = new GerenciadorDAO();
+        return client.listar();
+    }
+    //metodo excluir
+    public void remover(Gerenciador cliente){
+        GerenciadorDAO client = new GerenciadorDAO();
+        client.remover(cliente);
+    }
+    
+}
