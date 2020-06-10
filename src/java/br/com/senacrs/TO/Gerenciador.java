0572ec9ff7335be68e2b46a30ed783f2b19c0538
matheuslourenco-1 <57508423+matheuslourenco-1@users.noreply.github.com>
2020-06-10 02:03:36 -0300

    Removido Atributos que não faziam sentido ter e arrumado ConvertTime

--- src/java/br/com/senacrs/TO/Gerenciador.java
+++ src/java/br/com/senacrs/TO/Gerenciador.java
@@ -7,9 +7,6 @@ public class Gerenciador  implements java.io.Serializable {
 
     //declaração das variaveis 
      private Integer idGerenciador;
-     private String gerenciador_nomeFornecedor;
-     private String gerenciador_placaCaminhao;
-     private String gerenciador_nomeMotorista;
      private Date gerenciador_horarioEntrada;
      private Integer idMotorista;
      private Integer idCaminhao;
@@ -19,10 +16,7 @@ public class Gerenciador  implements java.io.Serializable {
          
      }
 
-    public Gerenciador(String gerenciador_nomeFornecedor, String gerenciador_placaCaminhao, String gerenciador_nomeMotorista, Date gerenciador_horarioEntrada) {
-        this.gerenciador_nomeFornecedor = gerenciador_nomeFornecedor;
-        this.gerenciador_placaCaminhao = gerenciador_placaCaminhao;
-        this.gerenciador_nomeMotorista = gerenciador_nomeMotorista;
+    public Gerenciador(Date gerenciador_horarioEntrada) {
         this.gerenciador_horarioEntrada = gerenciador_horarioEntrada;
     }
 
@@ -65,30 +59,6 @@ public class Gerenciador  implements java.io.Serializable {
 
     public void setIdGerenciador(Integer idGerenciador) {
         this.idGerenciador = idGerenciador;
-    }
-
-    public String getGerenciador_nomeFornecedor() {
-        return gerenciador_nomeFornecedor;
-    }
-
-    public void setGerenciador_nomeFornecedor(String gerenciador_nomeFornecedor) {
-        this.gerenciador_nomeFornecedor = gerenciador_nomeFornecedor;
-    }
-
-    public String getGerenciador_placaCaminhao() {
-        return gerenciador_placaCaminhao;
-    }
-
-    public void setGerenciador_placaCaminhao(String gerenciador_placaCaminhao) {
-        this.gerenciador_placaCaminhao = gerenciador_placaCaminhao;
-    }
-
-    public String getGerenciador_nomeMotorista() {
-        return gerenciador_nomeMotorista;
-    }
-
-    public void setGerenciador_nomeMotorista(String gerenciador_nomeMotorista) {
-        this.gerenciador_nomeMotorista = gerenciador_nomeMotorista;
     }
 
     public Date getGerenciador_horarioEntrada() {
